98d892a2e5d7096f13f52d853f32a0602c8c328c
Anil Niraula <niraulaanil@gmail.com>
2020-09-02 10:57:07 -0400

    Updated asset.alloc

--- Asset.Allocation.PERSI.R
+++ Asset.Allocation.PERSI.R
@@ -1,6 +1,34 @@
 #### PERSI Asset Allocation ####
-library(tidyr)
+rm(list=ls())
+###Load/install packages
+#R.Version()
+#https://github.com/ReasonFoundation/pensionviewr
+#Create token -> usethis::edit_r_environ() -> restart -> Sys.getenv("GITHUB_PAT")
+#install.packages('devtools')
+#library(devtools)
+#devtools::install_github("ReasonFoundation/reasontheme",force = TRUE)
+#devtools::install_github("ReasonFoundation/pensionviewr", force = TRUE)
+library(reasontheme)
+library(pensionviewr)
+#library(janitor)
+library(grid)#https://bookdown.org/rdpeng/RProgDA/the-grid-package.html
+library(tidyverse)
+#library(openxlsx)
+library(tseries)
+library(plyr)
+#library(ggplot2)
 library(data.table)
+library(openxlsx)
+#library(readr)
+library(rsconnect)
+library(base64enc)
+#Shiny-----------
+library(shiny)
+library(shinyWidgets)
+#library(shinyFiles)
+library(DT)
+library(plotly)
+#devtools::insta
 
 palette_reason <- data.table(
   Orange = "#FF6633", 
@@ -18,6 +46,14 @@ palette_reason <- data.table(
   LightRed = "#FF0000")
 
 
+plotTheme <- ggplot2::theme(   panel.grid.major = element_blank(),
+                               panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+                               plot.margin = margin(0, 0,0,0, "cm"),
+                               axis.text.y = element_text(size=9, color = "black"),
+                               axis.text.x = element_text(size=9, color = "black", angle = 0, hjust = 0.5, vjust = 0),
+                               legend.title = element_text(size = 9, colour = "white", face = "bold"))
+
+
 #Load G/L data for PERSI
 urlfile2="https://raw.githubusercontent.com/ReasonFoundation/GraphicsR/master/R/PERSI.Asset.Alloc.csv"
 PERSI.assets <- data.table(read_csv(url(urlfile2), col_names = TRUE, na = c(""), skip_empty_rows = TRUE, col_types = cols(.default = "n")))
@@ -28,7 +64,20 @@ PERSI.assets <- PERSI.assets %>% select(year, real.estate, alternatives, equity,
 
 #View(PERSI.assets)
 #Transpose datat from long ot wide format (each year contains Variable & Value)
-df.mlt <- melt(PERSI.assets, id.vars="year")
+PERSI.assets <- melt(PERSI.assets, id.vars="year")
 
-ggplot(df.mlt) + geom_area(aes(x = year, y = value, fill = variable, group = variable), position="fill")+
-  scale_fill_manual(values=c(palette_reason$Yellow,palette_reason$Orange, palette_reason$DarkGrey, palette_reason$LightBlue, palette_reason$SatBlue))
+ggplot(PERSI.assets) + geom_area(aes(x = year, y = value, fill = variable, group = variable), position="fill")+
+  scale_fill_manual(values=c(palette_reason$Yellow,palette_reason$Orange, 
+                             palette_reason$DarkGrey, palette_reason$LightBlue, palette_reason$SatBlue))+
+  scale_y_continuous(labels = function(x) paste0(x*100,"%"), name = "",
+                     breaks = seq(0, 1, by = 0.1), limits = c(0, 1))+
+  scale_x_continuous(labels = function(x) paste0(x, ""), name = "",
+                     breaks = seq(2001, 2019, by = 2), limits = c(2001, 2019))+
+  theme_bw()+
+  plotTheme+
+  theme(legend.position="bottom")
+  
+#############
+#############
+#############
+#############
