f1527629463cdf84aef9819376afb01d3029f917
Anil Niraula <niraulaanil@gmail.com>
2020-12-13 10:29:14 -0500

    Updated linePlot to areaPlot

---
+++ debtPlot2_ANedits.R
@@ -0,0 +1,183 @@
+##Updated debtPlot*(version ANedits)
+
+##############
+#HELLO!!!
+palette_reason
+rm(list=ls())
+###Load/install packages
+#R.Version()
+#https://github.com/ReasonFoundation/pensionviewr
+#Create token -> usethis::edit_r_environ() -> restart -> Sys.getenv("GITHUB_PAT")
+#install.packages('devtools')
+#library(devtools)
+#devtools::install_github("ReasonFoundation/reasontheme",force = TRUE)
+#devtools::install_github("ReasonFoundation/pensionviewr", force = TRUE)
+library(reasontheme)
+library(pensionviewr)
+library(ggplot2)
+library(tidyverse)
+library(tseries)
+library(data.table)
+library(readr)
+library(rsconnect)
+library(dplyr)
+library(plyr)
+
+#reason_color_pal()
+#library(extrafont)
+#font_import(pattern="Roboto")
+#loadfonts(device = "win", quiet = TRUE)
+##Palette
+#https://www.rapidtables.com/web/color/Web_Safe.html
+#https://www.colorbook.io/hexcolors/view/A69FA1
+
+#rgb1 <- col2rgb(palette_reason$Yellow)/255
+#rownames(rgb1) <- c("red", "green", "blue")
+#rgb1
+#Custom color code
+#ColorName <- rgb(0.2,0.2,0.4)
+#ColorName  
+
+###
+##Pull PERSI data
+##Load list of plans
+pl <- planList()
+
+#PULL DATA
+Data <- pullStateData(2001)
+Data <- filterData(Data,2001)
+#View(Data)
+PERSI.debt <- Data %>% filter(plan_name == "Idaho Public Employee Retirement System")
+PERSI.debt$year <- as.numeric(PERSI.debt$year)
+PERSI.debt$ava <- as.numeric(PERSI.debt$ava)
+PERSI.debt$aal <- as.numeric(PERSI.debt$aal)
+#Set to data.frame for visualization
+PERSI.debt <- data.table(PERSI.debt)
+PERSI.debt[,uaal := aal-ava]
+###########
+#set reasontheme
+set_reason_theme(style = "slide")
+tick <- c("axis.ticks = ggplot2::element_blank(),
+          axis.ticks.x = ggplot2::element_blank(),
+          axis.ticks.y = ggplot2::element_blank()")
+
+
+
+####Edit detPlot() manually
+############
+debtPlot <- function(data, sp500 = TRUE, title = NULL, caption = FALSE, grid = FALSE, ticks = TRUE, font) {
+  
+  if(sp500 == TRUE){
+    urlfile2="https://raw.githubusercontent.com/ReasonFoundation/databaseR/master/files/S%26P500.csv"
+    SP500 <- read_csv(url(urlfile2), col_names = TRUE, na = c(""), skip_empty_rows = TRUE, col_types = NULL)
+    
+    funded <- filteredData2(pl, "Idaho Public Employee Retirement System", start_year)
+    PERSI2.sp$year <- as.numeric(PERSI2.sp$year)
+    
+    # selecting the funded_ratio from the pl dataset
+    funded_ratio_y2 <- PERSI2.sp %>% select(year, funded_ratio)
+    
+    # make sure the SP500 range is correct> start_year
+    #SP500 <- SP500[SP500$year>=start_year,]
+    
+    # Merges the two dataframes SP500 and funded_ratio_y2
+    sp_data <- merge(SP500, funded_ratio_y2)
+  }
+  
+  data <- data %>%
+    dplyr::filter(data$uaal != 0)
+  # extrapolate between years linearly
+  extrapo <- stats::approx(data$year, data$uaal,  n = 10000)
+  extrapo2 <- stats::approx(data$year, data$funded_ratio, n = 10000)
+  graph <-
+    data.frame(year = extrapo$x,
+               uaal = extrapo$y,
+               funded_ratio = extrapo2$y) %>%
+    tidyr::drop_na()
+  graph <- graph %>%
+    dplyr::mutate(sign = dplyr::case_when(.data$uaal >= 0 ~ "positive",
+                                          .data$uaal < 0 ~ "negative"))
+  
+  y_minimum <- min(graph$uaal)
+  y_maximum <- max(graph$uaal)
+  ggplot2::ggplot(graph,
+                  ggplot2::aes(x = graph$year)) +
+    ggplot2::geom_area(ggplot2::aes(y = graph$uaal, fill = graph$sign)) +#Removed "color" parameter
+    ggplot2::geom_line(ggplot2::aes(y = graph$funded_ratio * (y_maximum)),
+                       color = palette_reason$GreyBlue,#Referenced Color Palette
+                       size = 1.7) +#Increased Size 1.
+    #ggtitle(title)+
+    # axis labels
+    ggplot2::labs(y = "Unfunded Accrued Actuarial Liabilities (Millions)", x = NULL) +
+    
+    # colors assigned to pos, neg
+    ggplot2::scale_fill_manual(
+      values = c("negative" = paste(palette_reason$Green),#Referenced Color Palette
+                 "positive" = paste(palette_reason$Red)),#Referenced Color Palette
+      aesthetics = c("colour", "fill")
+    ) +
+    # sets the y-axis scale
+    ggplot2::scale_y_continuous(
+      # creates 10 break points for labels
+      breaks = scales::pretty_breaks(n = 10),
+      # changes the format to be dollars, without cents, scaled to be in billions
+      labels = scales::dollar_format(
+        prefix = "$",
+        scale = (1e-6),
+        largest_with_cents = 1,
+      ), 
+      limits = c(y_minimum, y_maximum*1.2),
+      # defines the right side y-axis as a transformation of the left side axis, maximum UAAL = 100%, sets the breaks, labels
+      sec.axis = ggplot2::sec_axis(
+        ~ . / (y_maximum / 100),
+        breaks = scales::pretty_breaks(n = 10),
+        name = "Funded Ratio",
+        #set limits
+        labels = function(b) {
+          paste0(round(b, 0), "%")
+        }
+      ),
+      # removes the extra space so the fill is at the origin
+      expand = c(0, 0)
+    )+
+    geom_hline(yintercept=0, linetype="solid", color = "black", size = 0.5)+
+   ##Adding titles & caption
+    labs(title = paste(title), 
+         caption = ifelse(isTRUE(caption),paste("reason.org/pensions"),paste(""))
+    )+
+    ggplot2::theme(axis.ticks = if(isFALSE(ticks)){ggplot2::element_blank()}else{ggplot2::element_line()}
+                   )+
+    ggplot2::theme(axis.ticks.x = element_line(size = 0.5, color="black"))+
+    ggplot2::theme(axis.ticks.y = element_line(size = 0.5, color="black"))+
+   # coord_cartesian(ylim=(c(y_minimum, y_maximum*1.2)))+##Added limits
+    coord_cartesian(expand = FALSE, #turn off axis expansion (padding)
+                    xlim = c(2001, 2019), ylim = c(y_minimum, y_maximum*1.2))+ #manually set limits
+    # sets the x-axis scale
+    ggplot2::scale_x_continuous(breaks = round(seq(min(graph$year), max(graph$year), by = 2), 1),
+                                expand = c(0, 0)) +#Added blanck ticks to x-axis
+    
+    ggplot2::theme(legend.position = "none")+
+    ggplot2::theme(text = element_text(family = paste(font), size = 9))+ 
+    ##Adding Gridlines
+    ggplot2::theme(panel.grid.major.y = element_line(colour= ifelse(isTRUE(grid), 
+                           paste(palette_reason$SpaceGrey),"white"),size = (1)))
+}
+##Plot graph
+#debt.plot  <- debtPlot(PERSI.debt,font = "Calibri")
+#savePlot(debt.plot, source = "Source: PIP", save_filepath = "/Users/anilniraula/Downloads/test.png",
+#width_pixels = 600, height_pixels = 400)
+#With Title, caption and grid
+#debtPlot(PERSI.debt, "Idaho PERS Pension Debt", caption = TRUE, grid = TRUE, ticks = TRUE, font = "Verdana")
+
+debtPlot(PERSI.debt, caption = F, grid = F, ticks = F,font = "Calibri")
+
+############
+#https://github.com/bbc/bbplot/blob/master/R/finalise_plot.R
+#save_plot <- function (plot_grid, width, height, save_filepath) {
+#  grid::grid.draw(plot_grid)
+#  #save it
+#  ggplot2::ggsave(filename = save_filepath,
+#                  plot=plot_grid, width=(width/72), height=(height/72),  bg="white")
+#}
+#save_plot(plot1, 600, 400, "/users/Anil/downlaods")
+####
--- linePlot.R
+++ linePlot.R
@@ -3,6 +3,7 @@
 ## Base: linePlot() function in `pensionviewr`
 # By: Anil, Swaroop, and Jen
 
+###################
 rm(list=ls())
 ###Load/install packages
 #R.Version()
@@ -14,45 +15,36 @@ rm(list=ls())
 #devtools::install_github("ReasonFoundation/pensionviewr", force = TRUE)
 library(reasontheme)
 library(pensionviewr)
-library(ggplot2)
+#library(janitor)
+library(grid)#https://bookdown.org/rdpeng/RProgDA/the-grid-package.html
 library(tidyverse)
+#library(openxlsx)
 library(tseries)
-library(data.table)
-library(readr)
-library(rsconnect)
-library(dplyr)
 library(plyr)
-
-########
-##linePlot() function parameters:
-
-#data = data in data.frame, tibble, or data.table format,
-#yaxisMin & yaxisMax = Y-axis mininum and maximum values
-#yaxisSeq = set sequency of Major breaks
-#yaxisScale = 100 for percentages
-#format = set Y-axis format to either "%", "$", or empty
-#str = set number of srings at which to cut legend text (default is 20),
-#labelY = title of Y-axis,
-#lab1 - lab5 = character string of line titles/names (default is 0 Lines)
-
-###################
-#Using 2 New Functions to download and filter datat from datatabase
+#library(ggplot2)
+library(data.table)
+library(openxlsx)
+#library(readr)
+library(rsconnect)
+library(base64enc)
+#Shiny-----------
+library(shiny)
+library(shinyWidgets)
+library(shinyjs)
+#library(shinyFiles)
+library(DT)
+library(plotly)
+library(httr)
+library(jsonlite)
 
 PERSI.data <- pullStateData(2001)
 PERSI.data <- filterData(PERSI.data, 2001)
-reason.data <- pullStateData(2001)
-reason.data <- filterData(reason.data, 2001)
-
-pl <- planList()
-#filter for PERSI
 PERSI.data<- PERSI.data %>% filter (plan_name == "Idaho Public Employee Retirement System")
 #View(PERSI.data)
-
 PERSI.data$year <- as.numeric(PERSI.data$year)
 #Set to data.frame for visualization
 PERSI.data <- data.frame(PERSI.data)
 #View(PERSI.data)
-
 # UAL4 <- data.table(UAL4[, Tr30 := tr30[(n-UAL4[!is.na(Actual_Return),.N]):last]])
 ###############
 #Adding AVA returns (Arkansas ERS example*)
@@ -63,96 +55,100 @@ ava_returns <- data.table(ava_returns/100)
 
 PERSI.data <- data.table(PERSI.data)
 PERSI.data <- PERSI.data[, ava_return := ava_returns]
-
 PERSI.data$year <- as.numeric(PERSI.data$year)
-
 PERSI.data <- data.frame(PERSI.data)
 PERSI.data <- PERSI.data %>% select(year, return_1yr, ava_return, arr)
-
-#####Modified linePlot
-############
-
-linePlot <- function(data, yaxisMin = 0, yaxisMax = NULL, yaxisSeq = 5,
-                     yaxisScale = 100, format = NULL, str = 20,
-                     labelY = NULL, lab1 = NULL, 
-                     lab2 = NULL, lab3 = NULL, 
-                     lab4 = NULL, lab5 = NULL) {
-  
-  reasontheme::set_reason_theme(style = "slide")
-  
-  data <- data.table(data) %>% dplyr::mutate_all(dplyr::funs(as.numeric))
-  
-  
-  if(sum(data$return_1yr)>0){  
+#View(PERSI.data)
+#install.packages(c("httr", "jsonlite"))
+#devtools::install_github("ropensci/plotly")
+ 
+graph <- linePlot(PERSI.data, yaxisMin = -21, yaxisMax = 21, yaxisSeq = 3,
+                  yaxisScale = 100, format = "%", str = 60,
+                  labelY = "", lab1 = "Market Valued Returns (Actual)", 
+                  lab2 = "Actuarially Valued Investment Return (Smoothed by Plan)", 
+                  lab3 = "Assumed Rate of Return", 
+                  lab4 = "10-Year Geometric Rolling Average")
+graph
     
-    #####Adding GEOMEAN FUNCTION + CALCULATION if data contains "return_1yr" column
-    geomean <- function(x) {
-      x <- as.vector(na.omit(x))
-      x <- x +1
-      exp(mean(log(x)))-1 
-    }
+
+linePlot <- function (data, yaxisMin = 0, yaxisMax = NULL, yaxisSeq = 5, 
+                      yaxisScale = 100, format = NULL, str = 20, labelY = NULL, 
+                      lab1 = NULL, lab2 = NULL, lab3 = NULL, lab4 = NULL, lab5 = NULL) 
+{
+  reasontheme::set_reason_theme(style = "slide")
+  geomean <- function(x) {
+    x <- as.vector(na.omit(x))
+    x <- x + 1
+    exp(mean(log(x))) - 1
+  }
+  if (sum(!is.na(data$return_1yr)) > 0) {
     returns <- as.numeric(data$return_1yr)
     nyear <- 10
     rolling <- geomean(returns[1:nyear])
-    n <- length(na.omit(returns))-nyear
-    #Geomean function
-    for(i in 1:n){
-      rolling <- rbind(rolling, geomean(returns[(i+1):(i+nyear)]))
+    n <- length(na.omit(returns)) - nyear
+    for (i in 1:n) {
+      rolling <- rbind(rolling, geomean(returns[(i + 1):(i + 
+                                                           nyear)]))
     }
+    data <- data.frame(data) %>% dplyr::mutate_all(dplyr::funs(as.numeric))
     rolling <- data.table(rolling)
-    
     data <- data.table(rbind.fill(rolling, data))
-    data[(data[!is.na(return_1yr),.N]+1):(data[!is.na(return_1yr),.N]+rolling[,.N])]$V1<- data[(1:rolling[,.N])]$V1
-    data <- data[!(1:rolling[,.N])]
+    x <- data[!is.na(return_1yr), .N]
+    data[(x + 1):(x + rolling[, .N])]$V1 <- data[(1:rolling[, 
+                                                            .N])]$V1
+    data <- data[!(1:rolling[, .N])]
     data$year <- as.numeric(data$year)
-  }else{NULL}
-  
-  #Reshuffling column order per gemean calculated column named V1
-  if(sum(data$return_1yr)>0){ data <- data %>% select(year, return_1yr, ava_return, arr, V1)}
-  else{NULL}
-  
-  colnames(data) <- c("year", if(!is_null(lab1)){paste(lab1)},
-                      if(!is_null(lab2)){paste(lab2)},
-                      if(!is_null(lab3)){paste(lab3)},
-                      if(!is_null(lab4)){paste(lab4)},
-                      if(!is_null(lab5)){paste(lab5)})
-  
-  #Wide-to-Long format transformation for graphing
-  graph <- data.table(melt(data, id.vars="year"))
-  
-  lineColors <- c(palette_reason$Orange,palette_reason$Yellow, palette_reason$SatBlue,    palette_reason$LightGrey) #Updated palette to reason one
+    data <- data %>% select(year, return_1yr, ava_return, 
+                            arr, V1)
+  }
+  else {
+    data <- data.frame(data) %>% dplyr::mutate_all(dplyr::funs(as.numeric))
+  }
+  colnames(data) <- c("year", if (!is_null(lab1)) {
+    paste(lab1)
+  }, if (!is_null(lab2)) {
+    paste(lab2)
+  }, if (!is_null(lab3)) {
+    paste(lab3)
+  }, if (!is_null(lab4)) {
+    paste(lab4)
+  }, if (!is_null(lab5)) {
+    paste(lab5)
+  })
+  graph <- data.table(melt(data, id.vars = "year"))
+  lineColors <- c(palette_reason$Orange, palette_reason$Yellow, 
+                  palette_reason$SatBlue, palette_reason$LightGrey)
   options(repr.plot.width = 1, repr.plot.height = 0.75)
-  
-  ggplot2::ggplot(graph, ggplot2::aes(x = year, y = yaxisScale * value, group = variable)) +
-    ggplot2::geom_line(ggplot2::aes(colour = str_wrap(factor(variable), str)), size = 1.5) + #Added str_wrap(to cut legend text)
-    ggplot2::geom_hline(yintercept = 0, color = "black") +
-    ggplot2::scale_colour_manual(values = lineColors) +
-    ggplot2::scale_y_continuous(
-      breaks = seq(yaxisMin, if(!is.null(yaxisMax)){yaxisMax} #added automatic Y max calcualation
-                   else{max(graph$value)*yaxisScale*1.2}, by=yaxisSeq), 
-      limits = c(yaxisMin,if(!is.null(yaxisMax)){yaxisMax}
-                 else{max(graph$value)*yaxisScale*1.2}), #added limits and expanded break scale
-      labels = function(b) {
-        if(format == "%"){
-          paste0(round(b, 0), "%")
-        }else if(format == "$"){#Adding "$" format
-          paste0("$", round(b, 0))
-        }else{
-          paste0(format, round(b, 0))}
-      },
-      expand = c(0, 0)
-    ) +
-    
-    ggplot2::scale_x_continuous(breaks = seq(min(graph$year), max(graph$year), by = 2), #added blank years
-                                expand = c(0, 0)
-    ) +
-    
-    labs(x = element_blank(), y = labelY)+
-    theme(legend.text=element_text(size=13))+ #Added element to control legend font size
-    theme(legend.direction = "vertical", #changing legend direction to "vertical"
-          legend.box = "horizontal",
-          legend.position= c(0.33, 0.09)) #Moved legend to the bottom
+  ggplot2::ggplot(graph, ggplot2::aes(x = year, y = yaxisScale * 
+                                        value, group = variable)) + ggplot2::geom_line(ggplot2::aes(colour = str_wrap(factor(variable), 
+                                                                                                                      str)), size = 1.5) + ggplot2::geom_hline(yintercept = 0, 
+                                                                                                                                                               color = "black") + ggplot2::scale_colour_manual(values = lineColors) + 
+    ggplot2::scale_y_continuous(breaks = seq(yaxisMin, if (!is.null(yaxisMax)) {
+      yaxisMax
+    }
+    else {
+      max(graph$value) * yaxisScale * 1.2
+    }, by = yaxisSeq), limits = c(yaxisMin, if (!is.null(yaxisMax)) {
+      yaxisMax
+    } else {
+      max(graph$value) * yaxisScale * 1.2
+    }), labels = function(b) {
+      if (format == "%") {
+        paste0(round(b, 0), "%")
+      }
+      else if (format == "$") {
+        paste0("$", round(b, 0))
+      }
+      else {
+        paste0(format, round(b, 0))
+      }
+    }, expand = c(0, 0)) + ggplot2::scale_x_continuous(breaks = seq(min(graph$year), 
+                                                                    max(graph$year), by = 2), expand = c(0, 0)) + labs(x = element_blank(), 
+                                                                                                                       y = labelY) + theme(legend.text = element_text(size = 13)) + 
+    theme(legend.direction = "vertical", legend.box = "horizontal", 
+          legend.position = c(0.33, 0.09))
 }
+
 
 graph <- linePlot(PERSI.data, yaxisMin = -21, yaxisMax = 21, yaxisSeq = 3,
                   yaxisScale = 100, format = "%", str = 60,
